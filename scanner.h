6af9a12a0c17d628c52a94a2d40f757de63628b7
Corrina Lakin <corrina.lakin@gmail.com>
2022-02-23 14:51:30 -0600

    error with string

--- scanner.h
+++ scanner.h
@@ -1,14 +1,20 @@
+// #ifndef SCANNER_h
+// #define SCANNER_h
+
+// #include "testScanner.h"
 
 
+// #define KW 1000;
+// #define ID 1001;
+// #define EOF 1002;
+// #define NUM 1003;
+// #define OP 1004;
+// #define CMT 1005;
+// #define ERR -1;
 
-#define KW 1000;
-#define ID 1001;
-#define EOF 1002;
-#define NUM 1003;
-#define OP 1004;
-#define CMT 1005;
-#define ERR -1;
+void scanner(string&, int);
+char getChar(string&, int);
+//tokenType FSDriver();
 
-void scanner(string, int);
-char getChar(string, int);
-tokenType FSDriver();
+
+// #endif SCANNER_h
